444ed0bee43c482e4767f67914cfd075e224b43b
jai1007-lab <74004879+jai1007-lab@users.noreply.github.com>
2021-09-04 16:53:05 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -2,7 +2,9 @@ public class AddTwoNumbers {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, sum;
+      int num1 = 5, 
+      int num2 = 15, 
+      int sum;
       sum = num1 + num2;
 
       System.out.println("Sum of these numbers: "+sum);
